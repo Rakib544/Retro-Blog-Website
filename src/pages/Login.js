0286b347513b5d26447f69b7f41c0f2c7a8c9183
Rakib544 <md.rakib10122003@gmail.com>
2021-06-24 16:15:14 +0600

    receive data from login and register done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const { register, handleSubmit, error } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <React.Fragment>
             <div className="w-full md:w-9/12 lg:w-1/2 mx-auto">
                 <h2 className="text-2xl font-bold text-center my-6">Login Here</h2>
-                <form className="px-2 md:px-10">
+                <form
+                    className="px-2 md:px-10"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <label htmlFor="Email" className="text-md m-1 block text-gray-700">
                         Email
                     </label>
@@ -13,6 +19,7 @@ const Login = () => {
                         type="text"
                         placeholder="Enter Email"
                         name="email"
+                        ref={register({required: true})}
                         className="w-full rounded-full py-3 px-8 ring-0 focus:outline-none border border-gray-700"
                     />
                     <label htmlFor="password" className="text-md m-2 block">
@@ -22,6 +29,7 @@ const Login = () => {
                         type="password"
                         placeholder="Enter Password"
                         name="password"
+                        ref={register({required: true})}
                         className="w-full rounded-full py-3 px-8 ring-0 focus:outline-none border border-gray-700"
                     />
                     <button
