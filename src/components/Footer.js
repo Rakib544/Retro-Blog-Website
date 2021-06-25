8c78f242cda52c2b73488d6369352ff2b7aae67e
Rakib544 <md.rakib10122003@gmail.com>
2021-06-26 01:38:46 +0600

    some code updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,32 +2,39 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="grid grid-cols-1 md:grid-cols-5 gap-4 px-8 text-center bg-primary py-12 text-white">
-            <div>
-                <h1>Logo</h1>
-                <p>Retro Company</p>
+        <>
+            <footer 
+                className="grid grid-cols-1 md:grid-cols-5 gap-4 px-8 text-center bg-primary py-12 text-gray-400 uppercase text-xs font-medium"
+            >
+                <div>
+                    <h1 className="mb-2 cursor-pointer">Logo</h1>
+                    <p className="mb-2 cursor-pointer">Retro Company</p>
+                </div>
+                <div>
+                    <p className="mb-2 cursor-pointer">Weebly Themes</p>
+                    <p className="mb-2 cursor-pointer">Pre-sales Faqs</p>
+                    <p className="mb-2 cursor-pointer">Submit a ticket</p>
+                </div>
+                <div>
+                    <p className="mb-2 cursor-pointer">Services</p>
+                    <p className="mb-2 cursor-pointer">Theme Week</p>
+                </div>
+                <div>
+                    <p className="mb-2 cursor-pointer">ShowCase</p>
+                    <p className="mb-2 cursor-pointer">Widgekit</p>
+                    <p className="mb-2 cursor-pointer">support</p>
+                </div>
+                <div>
+                    <p className="mb-2 cursor-pointer">About us</p>
+                    <p className="mb-2 cursor-pointer">Contact Us</p>
+                    <p className="mb-2 cursor-pointer">Affiliates</p>
+                    <p className="mb-2 cursor-pointer">recourses</p>
+                </div>
+            </footer>
+            <div className="py-8 bg-secondary text-center text-gray-300 text-xs tracking-wider">
+                &copy;Copyright by Md Rakib
             </div>
-            <div>
-                <p>Weebly Themes</p>
-                <p>Pre-sales Faqs</p>
-                <p>Submit a ticket</p>
-            </div>
-            <div>
-                <p>Services</p>
-                <p>Theme Week</p>
-            </div>
-            <div>
-                <p>ShowCase</p>
-                <p>Widgekit</p>
-                <p>support</p>
-            </div>
-            <div>
-                <p>About us</p>
-                <p>Contact Us</p>
-                <p>Affiliates</p>
-                <p>recourses</p>
-            </div>
-        </footer>
+        </>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         primary: '#1F263E',
+        secondary: '#1B2132',
+        info: '#F8F9FA',
       },
       fontFamily: {
         body: ['Poppins']
