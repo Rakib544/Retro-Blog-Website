edee8d9d547e3300c0f7d7f99cc2ea41cf91602a
Rakib544 <md.rakib10122003@gmail.com>
2021-06-26 10:37:23 +0600

    footer and scrollbar customization deon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,32 +3,38 @@ import React from 'react';
 const Footer = () => {
     return (
         <>
-            <footer 
+            <footer
                 className="grid grid-cols-1 md:grid-cols-5 gap-4 px-8 text-center bg-primary py-12 text-gray-400 uppercase text-xs font-medium"
             >
                 <div>
-                    <h1 className="mb-2 cursor-pointer">Logo</h1>
-                    <p className="mb-2 cursor-pointer">Retro Company</p>
+                    <img
+                        src="https://retrolie.thememove.com/wp-content/themes/retrolie/assets/images/light-logo@2x.png"
+                        className="w-24 block mx-auto" alt="logo"
+                    />
+                    <p className="my-2 cursor-pointer">Retro Company</p>
                 </div>
                 <div>
+                    <h2 className="text-white text-md font-bold mb-4">Quick Links</h2>
                     <p className="mb-2 cursor-pointer">Weebly Themes</p>
                     <p className="mb-2 cursor-pointer">Pre-sales Faqs</p>
                     <p className="mb-2 cursor-pointer">Submit a ticket</p>
                 </div>
                 <div>
+                    <h2 className="text-white text-md font-bold mb-4">Services</h2>
                     <p className="mb-2 cursor-pointer">Services</p>
                     <p className="mb-2 cursor-pointer">Theme Week</p>
                 </div>
                 <div>
+                    <h2 className="text-white text-md font-bold mb-4">Support</h2>
                     <p className="mb-2 cursor-pointer">ShowCase</p>
                     <p className="mb-2 cursor-pointer">Widgekit</p>
                     <p className="mb-2 cursor-pointer">support</p>
                 </div>
                 <div>
-                    <p className="mb-2 cursor-pointer">About us</p>
-                    <p className="mb-2 cursor-pointer">Contact Us</p>
-                    <p className="mb-2 cursor-pointer">Affiliates</p>
-                    <p className="mb-2 cursor-pointer">recourses</p>
+                    <h2 className="text-white text-md font-bold mb-4">Contact Us</h2>
+                    <p className="mb-2 cursor-pointer">Gazipur, Dhaka, Bangladesh</p>
+                    <p className="mb-2 cursor-pointer">dev.rakib01@gmail.com</p>
+                    <p className="mb-2 cursor-pointer">01786542643</p>
                 </div>
             </footer>
             <div className="py-8 bg-secondary text-center text-gray-300 text-xs tracking-wider">
